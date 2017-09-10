8e085698423b32c1128859182b684e40d0427494
Matt C <matt.cheah@gmail.com>
2017-09-10 16:50:34 -0700

    edit app.js to only play video that matches the name you click.

--- app.js
+++ app.js
@@ -13,40 +13,46 @@ app.get('/get-videos/:magnet', function(req, res) {
   var decoded = new Buffer(req.params.magnet, 'base64').toString('ascii')
   var filesArray = [];
   engine = torrentStream(decoded);
-  console.log("created get video engine");
   engine.on('ready', function() {
       engine.files.forEach(function(file) {
           filesArray.push(file.name);
       });
-      console.log("filesarray");
-      console.log(filesArray);
       res.send(filesArray);
   });
   
 });
 
-app.get('/streamMagnet/:magnet', function (req, res) {
-console.log('Starting new stream');
-var decoded = new Buffer(req.params.magnet, 'base64').toString('ascii')
-engine = torrentStream(decoded);
+app.get('/streamMagnet/:magnet/:name', function (req, res) {
+  console.log('Starting new stream');
+  var decoded = new Buffer(req.params.magnet, 'base64').toString('ascii')
+  if (req.params.magnet) {
+    var name = Buffer.from(req.params.name, 'base64').toString();
+  }
+  engine = torrentStream(decoded);
 
-engine.on('ready', function() {
+  engine.on('ready', function() {
     engine.files.forEach(function(file) {
-	var containMKV = file.name.includes('.mkv');
-	var containMP4 = file.name.includes('.mp4');
+      if (name) {
+          if (name == file.name) {
+            console.log("Playing: "+name);
+            file.createReadStream().pipe(res);
+          }
+      } else {
+        var containMKV = file.name.includes('.mkv');
+        var containMP4 = file.name.includes('.mp4');
         if(containMP4 == true || containMKV == true) {
-  			file.createReadStream().pipe(res);
-		    }
-        else{
+          file.createReadStream().pipe(res);
+        } else {
           //Do nothing as it as unstreamable file.
         }
+      }
     });
-});
+  });
 })
 
 app.get('/endstream', function(req, res) {
 	engine.destroy(function() {
-		res.send('sucess');
+		res.send('success');
 	});
 });
 
